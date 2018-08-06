4b411e787edd36b6f9b51c452c34736892ca08f2
C. H. Lay <development@cschlay.com>
2018-08-06 10:29:37 +0300

    Quick start for new notexbook.

---
+++ startbook.py
@@ -0,0 +1,25 @@
+import sys
+
+argv = sys.argv
+book_name = argv[1]
+author = argv[2]
+
+# Set the path (inclding directory) to which create a new notebook.
+LOCATION = 'C:/' + book_name + '/main.tex'
+
+file = open(LOCATION, 'w+')
+
+content = [
+    '\document{notexbook-full}\n\n',
+    '\\title{', book_name, '}\n',
+    '\\author{', author, '}\n\n',
+    '\\begin{document}\n',
+    ''.ljust(4), '\maketitle\n',
+    ''.ljust(4), '\\tableofcontents\n',
+    '\end{document}'
+]
+
+for line in content:
+    file.write(line)
+
+file.close()
